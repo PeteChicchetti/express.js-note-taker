5dd90b4295aa81d2c1bea79b3b861ff7e413cf55
PeteChicchetti <pj0524@gmail.com>
2022-10-29 16:10:07 -0400

    corrected typo to get route

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) =>
 );
 
 // GET Route to the notes page
-app.get('/feedback', (req, res) =>
+app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
