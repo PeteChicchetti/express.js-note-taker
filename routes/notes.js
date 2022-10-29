cc9c49590daa2546c6b4591d0601379cdc6d3c5c
PeteChicchetti <pj0524@gmail.com>
2022-10-29 16:08:23 -0400

    added variable/if write location and response

--- routes/notes.js
+++ routes/notes.js
@@ -12,4 +12,29 @@ notes.post('/', (req, res) => {
     // Console log that a POST request was received
     console.info(`${req.method} request received to save notes`);
 
+    // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+    };
+
+    readAndAppend(newNote, './db/db.json');
+
+    const response = {
+        status: 'success, new note created',
+        body: newNote,
+      };
+  
+      res.json(response);
+    } else {
+      res.json('Error posting note');
+    }
+
 });
+
+module.exports = notes;
