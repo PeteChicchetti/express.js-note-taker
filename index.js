294e1b3a9a8085db936523182c94a6c985406428
PeteChicchetti <pj0524@gmail.com>
2022-10-29 15:56:00 -0400

    added local server to index.js in main

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js');
+// Port for localhost
+const PORT = 3001;
+// Variable for using express
+const app = express();
+
+// Middleware to parse JSON and urlencoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Route to api
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET Route to the homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route to the notes page
+app.get('/feedback', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// LocalHost
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} `)
+);
