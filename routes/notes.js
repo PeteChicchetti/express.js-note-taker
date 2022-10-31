475f2a25ff593fc7a207c2aaf321ade04f287f3c
PeteChicchetti <pj0524@gmail.com>
2022-10-31 18:42:45 -0400

    comment out delete

--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,7 @@
 const notes = require('express').Router();
 const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
-
+const db = require('../db/db.json')
 
 // GET Route for retrieving all the feedback
 notes.get('/', (req, res) => {
@@ -12,7 +12,7 @@ notes.get('/', (req, res) => {
 
 // POST Route for submitting notes
 notes.post('/', (req, res) => {
-
+  console.info(`${req.method} request received for `);
     // Destructuring assignment for the items in req.body
   const { title, text } = req.body;
 
@@ -40,5 +40,21 @@ notes.post('/', (req, res) => {
 
 });
 
+// notes.delete('/:id', (req, res) => {
+//   const deleteId = JSON.parse(req.params.id);
+//   readFileAsync('./db/db.json', 'utf8').then(function(data) {
+//     const notes = [].concat(JSON.parse(data));
+//     const newNotes = []
+//     for (let i = 0; i < notes.length; i++) {
+//       if(deleteId !== notes[i].id) {
+//         newNotes.push(notes[i])
+//       }
+//     }
+//     return newNotes
+//   }).then(function (notes) {
+//     writeFileAsync('../db/db.json', JSON.stringify(notes))
+//     res.send('notes updated')
+//   })
+// });
 
 module.exports = notes;
