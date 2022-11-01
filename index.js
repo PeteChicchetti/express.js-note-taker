206a22320e9d08dc01fd5d85f25c5a0126d0d41a
PeteChicchetti <pj0524@gmail.com>
2022-11-01 15:10:24 -0400

    added port for Heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/index.js');
 // Port for localhost
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 // Variable for using express
 const app = express();
 
